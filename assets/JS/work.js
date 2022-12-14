a55f69760f39717a74243900142c5e2585edd569
CougarChan <gohan0136@gmail.com>
2022-12-14 12:49:34 -0500

    Final

--- assets/JS/work.js
+++ assets/JS/work.js
@@ -1,57 +1,125 @@
-// Display today's day and date
-var todayDate = moment().format('dddd, MMM Do YYYY');
-$("#currentDay").html(todayDate);
+$(document).ready(function() {
 
-$(document).ready(function () {
-    // saveBtn click listener 
-    $(".saveBtn").on("click", function () {
-        // Get nearby values of the description in JQuery
-        var text = $(this).siblings(".description").val();
-        var time = $(this).parent().attr("id");
-
-        // Save text in local storage
-        localStorage.setItem(time, text);
-    })
-   
-    function timeTracker() {
-        //get current number of hours.
-        var timeNow = moment().hour();
-
-        // loop over time blocks
-        $(".time-block").each(function () {
-            var blockTime = parseInt($(this).attr("id").split("hour")[1]);
-
-            // To check the time and add the classes for background indicators
-            if (blockTime < timeNow) {
-                $(this).removeClass("future");
-                $(this).removeClass("present");
-                $(this).addClass("past");
+    // Create html of the timeblocks
+    
+    for (var i = 9; i <= 17; i++){
+        //$('.container').append(`<div class="row time-block" data-time="${i}"> 
+        //<div class="col-sm col-md-2 hour"> 
+          //  <p>${i}00hrs</p> 
+        //</div> 
+        //<div class="col-sm col-md-8 d-flex description"> 
+           // <textarea></textarea> 
+        //</div> 
+        //<div class="col-sm col-md-2 saveBtn"> 
+          //  <i class="far fa-save fa-2x"></i> 
+        //</div> 
+        //</div>`);
+        if (i <= 11){
+            var t1 = i + ":00 "
+            $('.container').append(`<div class="row time-block" data-time="${i}">
+                    <div class="col-sm col-md-2 hour">
+                            <p>${t1}AM</p>
+                    </div>
+                    <div class="col-sm co;-md-8 d-flex description">
+                        <textarea></textarea>
+                    </div><div class="col-sm con-md-2 saveBtn">
+                            <i class="far fa-save fa-2x"></i>
+                        </div></>
+                </div>`);
+        }
+        else if(i == 12){
+                var t2 = i +":00 "
+                $('.container').append(`<div class="row time-block" data-time="${i}">
+                    <div class="col-sm col-md-2 hour">
+                            <p>${t2}PM</p>
+                    </div>
+                    <div class="col-sm co;-md-8 d-flex description">
+                        <textarea></textarea>
+                    </div><div class="col-sm con-md-2 saveBtn">
+                            <i class="far fa-save fa-2x"></i>
+                        </div></>
+                </div>`);
             }
-            else if (blockTime === timeNow) {
-                $(this).removeClass("past");
-                $(this).removeClass("future");
-                $(this).addClass("present");
+        else{
+            var t3 = (i - 12)+":00 ";
+            $('.container').append(`<div class="row time-block" data-time="${i}">
+                    <div class="col-sm col-md-2 hour">
+                            <p>${t3}PM</p>
+                    </div>
+                    <div class="col-sm co;-md-8 d-flex description">
+                        <textarea></textarea>
+                    </div><div class="col-sm con-md-2 saveBtn">
+                            <i class="far fa-save fa-2x"></i>
+                        </div></>
+                </div>`);
             }
-            else {
-                $(this).removeClass("present");
-                $(this).removeClass("past");
-                $(this).addClass("future");
-
-            }
-        })
     }
-
-    // Get item from local storage if any
-    $("#hour8 .description").val(localStorage.getItem("hour8"));
-    $("#hour9 .description").val(localStorage.getItem("hour9"));
-    $("#hour10 .description").val(localStorage.getItem("hour10"));
-    $("#hour11 .description").val(localStorage.getItem("hour11"));
-    $("#hour12 .description").val(localStorage.getItem("hour12"));
-    $("#hour13 .description").val(localStorage.getItem("hour13"));
-    $("#hour14 .description").val(localStorage.getItem("hour14"));
-    $("#hour15 .description").val(localStorage.getItem("hour15"));
-    $("#hour16 .description").val(localStorage.getItem("hour16"));
-    $("#hour17 .description").val(localStorage.getItem("hour17"));
-
-    timeTracker();
-})
+    
+    
+    let timeTrackObject = {};
+        //Checks if local storage exists, if it doesn't load preset data to array.<fixed>
+        if (localStorage.getItem('timeTrackObject')) {
+            timeTrackObject = JSON.parse(localStorage.getItem('timeTrackObject'));
+        }else{
+            timeTrackObject = {
+                '9': { time: "9", value: ""},
+                '10':{ time: "10", value: ""},
+                '11':{ time: "11", value: ""},
+                '12':{ time: "12", value: ""},
+                '13':{ time: "13", value: ""},
+                '14':{ time: "14", value: ""},
+                '15':{ time: "15", value: ""},
+                '16':{ time: "16", value: ""},
+                '17':{ time: "17", value: ""}
+            };
+        }
+    
+    //Load data loaded from code under comment 1 into page <fixed>
+    $(".time-block").each(function(){
+        $(this).find(".description textarea").val(timeTrackObject[$(this).attr("data-time")].value);
+    });
+    
+    // moment.js to show what day it is.
+    let dateString = moment().format('dddd') + ", " +moment().format("MMM Do YY");
+    $("#currentDay").html(dateString.substring(0, dateString.length - 5) + "th");
+    
+    //checks the hour of the current day to the hour for HTML data-element to decide it's background color.
+    const m = moment();
+    $.each($(".time-block"), function(index, value){
+        let dateHour = $(value).attr("data-time");
+        if (Number(dateHour) === m.hour()) {
+            $(this).find("textarea").addClass('present');
+        } else if(Number(dateHour) < m.hour()){
+            $(this).find("textarea").addClass('past');
+        } else {
+            $(this).find("textarea").addClass('future');
+        } 
+    });
+    
+    //When a user clicks the save button data is saved to the objects 
+    //and to local storage changing the data loaded in new sessions.
+    $("body").on('click', ".saveBtn", function(e){
+    
+    //Sets variables for calling data
+    let hour = $(this).closest(".time-block").attr("data-time");
+    let textValue = $(this).closest(".time-block").find(".description textarea").val();
+    //let day = moment().format("MMM Do YY").substring(0,5);
+    
+    // Value is overrided by for the objects value
+    timeTrackObject[hour].value = textValue;
+    
+    //Sends value to local storage for later use.
+    localStorage.setItem('timeTrackObject', JSON.stringify(timeTrackObject));
+    
+    });
+    
+    // Button to clear all data.
+    
+    $("body").on('click', "#clearData", function(e){
+        localStorage.setItem('timeTrackObject', "");
+        $(".time-block").each(function(){
+            $(this).find(".description textarea").val('');
+        });
+    });
+    
+});
